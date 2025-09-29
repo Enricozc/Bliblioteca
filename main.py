26398f53c6640cc7b8ad189f55dfccad2bd18d2b
EnricoZC <enrico.candido08@gmail.com>
2025-09-29 14:48:33 -0300

    listagem

--- main.py
+++ main.py
@@ -23,3 +23,19 @@ def cadastrar_livro(titulo, autor, ano):
     conexao.commit()
     print(f" Livro '{titulo}' cadastrado com sucesso!")
 
+
+def listar_livros():
+
+    cursor.execute("SELECT id, titulo, autor, ano, disponivel FROM livros")
+    livros = cursor.fetchall()
+
+    if not livros:
+        print("Nenhum livro cadastrado ainda.")
+        return
+
+    print("\n Lista de Livros Cadastrados:")
+    print("-" * 60)
+    for livro in livros:
+        print(f"ID: {livro[0]} | Título: {livro[1]} | Autor: {livro[2]} | Ano: {livro[3]} | Disponível: {livro[4]}")
+    print("-" * 60)
+
